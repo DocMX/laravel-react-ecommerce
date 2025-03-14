91d0667e37f9acde8702340a37f518f91b943f2d
DocMX <jorge_cruzer@hotmail.com>
2025-03-13 18:47:35 -0600

    changes in index d ts and changes in typographical errors

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,17 +1,42 @@
 import { Config } from 'ziggy-js';
+import { LucideIcon } from 'lucide-react';
 
 export interface User {
     id: number;
     name: string;
     email: string;
     email_verified_at?: string;
+    avatar?: string;
 }
 
-export type PageProps<
-    T extends Record<string, unknown> = Record<string, unknown>,
-> = T & {
+export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
     auth: {
         user: User;
     };
     ziggy: Config & { location: string };
 };
+
+export interface BreadcrumbItem {
+    title: string;
+    href: string;
+}
+
+export interface NavGroup {
+    title: string;
+    items: NavItem[];
+}
+
+export interface NavItem {
+    title: string;
+    href: string;
+    icon?: LucideIcon | null;
+    isActive?: boolean;
+}
+
+export interface SharedData {
+    name: string;
+    quote: { message: string; author: string };
+    auth: Auth;
+    ziggy: Config & { location: string };
+    [key: string]: unknown;
+}
