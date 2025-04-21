348104ded95e27fbdc222e4e3930ee28b0604ad3
DocMX <jorge_cruzer@hotmail.com>
2025-04-21 10:02:42 -0600

    Success for stripe

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -16,8 +16,14 @@ class StripeController extends Controller
 
     }
 
-    public function webhook()
+    public function webhook(Request $request)
     {
+        $stripe = new \Stripe\StripeClient(config('app.stripe_secret_key'));
 
+        $endpoint_secret = config('app.stripe_endpoint_secret');
+
+        $payload = $request->getContent();
+        $sig_header = request()->header('Stripe_Signature');
+        $event = null;
     }
 }
