567970608994c367d99fa15d05545091731fd66f
DocMX <jorge_cruzer@hotmail.com>
2025-04-21 11:04:32 -0600

    webhook in stripe contoller events

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class StripeController extends Controller
 {
@@ -25,5 +26,35 @@ class StripeController extends Controller
         $payload = $request->getContent();
         $sig_header = request()->header('Stripe_Signature');
         $event = null;
+
+        try {
+            $event = \Stripe\Webhook::constructEvent(
+                $payload, $sig_header, $endpoint_secret
+            );
+        } catch (\UnexpectedValueException $e) {
+           Log::error($e);
+           //invalid payload
+           return response('Invalid Payload', 400);
+        } catch(\Stripe\Exception\SignatureVerificationException $e){
+            Log::error($e);
+            return response('Invalid Payload' , 400);
+        }
+
+        Log::info('================================');
+        Log::info('================================');
+        Log::info($event->type);
+        Log::info($event);
+
+        //Handle the Event
+        switch ($event->type) {
+            case 'carg.updated':
+                $charge = $event->data->object;
+                $transactionId = $charge['balance_transaction'];
+                $paymentIntent = $charge['payment_intent'];
+                // Send email to buyer
+            case 'checkout.session.completed':
+       
+        }
+
     }
 }
