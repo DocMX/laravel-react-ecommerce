13867c6343f8a00bd9d120b7219f3effd36ff5e0
DocMX <jorge_cruzer@hotmail.com>
2025-04-27 21:40:34 -0600

    webhook by stripe completed

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\OrderStatusEnum;
+use App\Models\CartItem;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -55,9 +57,76 @@ class StripeController extends Controller
                     ->get();
                 $totalAmount = $balanceTransaction['amount'];
                 $stripeFee = 0;
+                foreach ($balanceTransaction['fee_details'] as $fee_detail) {
+                    if ($fee_detail['type'] === 'stripe_fee') {
+                        $stripeFee = $fee_detail['amount'];
+                    }
+                }
+                $platformFreePercent = config('app.platform_fee_pct');
+
+
+                foreach ($orders as $order) {
+                    $vendorShare = $order->total_price / $totalAmount;
+
+                    $order -> online_payment_commission = $vendorShare * $stripeFee;
+                    $order -> website_commission = ($order->total_price - $order->online_payment_commission)/ 100 * $platformFreePercent;
+                    $order->vendor_subtotal = $order->total_price - $order->online_payment_commission - $order -> website_commission;
+
+                    $order->save();
+                    //Todo send email to vendor
+                }
                 // Send email to buyer
             case 'checkout.session.completed':
+                $session = $event->data->object;
+                $pi = $session['payment_intent'];
+
+                //Find orders by session ID and set payment intent
+                $orders = Order::query()
+                    ->with(['orderItems'])
+                    ->where(['stripe_session_id' => $session['id']])
+                    ->get();
+                
+                $productsToDeletedFromCart = [];
+                foreach ($orders as $order ) {
+                    $order -> payment_intent = $pi;
+                    $order -> status = OrderStatusEnum::Paid;
+                    $order -> save();
+
+                    $productsToDeletedFromCart = 
+                        [
+                            ...$productsToDeletedFromCart,
+                            ...$order->orderItems->map(fn($item) => $item->product_id)->toArray()
+                        ];
+
+                    //Reduce product quantity
+                    foreach ($order->orderItems as $orderItem) {
+                        $options = $orderItem -> variation_type_option_ids; 
+                        $product = $orderItem -> product;
+                        if ($options) {
+                            sort($options);
+                            $variation = $product->variations()
+                                ->where('variation_type_option_ids', $options)
+                                ->first();
+                            if ($variation && $variation ->quantity != null) {
+                                $variation->quantity-= $orderItem->quantity;
+                                $variation->save();
+                            }
+                        }elseif ($product->quantity != null) {
+                            $product->quantity-=$orderItem->quantity;
+                            $product->save();
+                        }
+                    }
+                }
+
+                CartItem::query()
+                    ->where('user_id', $order->user_id)
+                    ->whereIn('product_id',  $productsToDeletedFromCart)
+                    ->where('saved_for_later', false)
+                    ->delete();
+            default:
+                echo 'Received unknow event type' . $event->type;
         }
+        return response('', 200);
     }
     
 }
--- config/app.php
+++ config/app.php
@@ -127,5 +127,6 @@ return [
 
     'stripe_secret_key' => env('STRIPE_SECRET'),
     'stripe_webhook_secret' => env('STRIPE_WEBHOOK_SECRET'),
+    'platform_fee_pct' => 10,
 
 ];
